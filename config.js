6ee0a956fb6c4424cdb52b2d18d5d625cd74e49c
Taylor Young <tdy721@gmail.com>
2015-01-17 23:01:22 -0600

    tablesaw

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 module.exports = {
-	title: "Reap Chart Maker"
+	title: "Reap Table Maker"
 	, description: "A Reap Marketing Application"
-	, heading: "Report Maker 1.0"
+	, heading: "Table Maker Demo"
 	, javascripts: [
 		'bower_components/jquery/dist/jquery.js'
 		, 'bower_components/jsviews/jsviews.js'
-		, 'bower_components/chartist/dist/chartist.js'
+		, 'bower_components/tablesaw/dist/tablesaw.js'
 		, 'bower_components/pace/pace.js'
 		, 'dev/js/script.js'
 	]
 	, stylesheets: [
 		'bower_components/bootstrap/dist/css/bootstrap.css'
 		, 'bower_components/pace/themes/green/pace-theme-barber-shop.css'
-		, 'bower_components/chartist/dist/chartist.min.css'
+		, 'bower_components/tablesaw/dist/tablesaw.css'
 		, 'dev/css/futura.css'
 		, 'dev/css/style.css'
 	]
