d83bb3f498adb793ece1ebe3f8d5b0e826eb2657
Taylor Young <tdy721@gmail.com>
2015-01-20 15:37:03 -0600

    added chartist dependency

--- config.js
+++ config.js
@@ -1,19 +1,22 @@
 module.exports = {
-	title: "Reap Table Maker"
+	title: "Reap Report Maker"
 	, description: "A Reap Marketing Application"
-	, heading: "Table Maker Demo"
+	, heading: "Report Maker Beta 0.5.0"
 	, javascripts: [
 		'bower_components/jquery/dist/jquery.js'
 		, 'bower_components/jsviews/jsviews.js'
 		, 'bower_components/tablesaw/dist/tablesaw.js'
+		, 'bower_components/chartist/dist/chartist.js'
 		, 'bower_components/pace/pace.js'
 		, 'dev/js/tmpl.js'
+		, 'dev/js/chart.js'
 		, 'dev/js/script.js'
 	]
 	, stylesheets: [
 		'bower_components/bootstrap/dist/css/bootstrap.css'
 		, 'bower_components/pace/themes/green/pace-theme-barber-shop.css'
 		, 'bower_components/tablesaw/dist/tablesaw.css'
+		, 'bower_components/chartist/dist/chartist.min.css'
 		, 'dev/css/futura.css'
 		, 'dev/css/style.css'
 	]
