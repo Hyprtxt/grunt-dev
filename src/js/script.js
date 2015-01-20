e66d91b2aeca363c30c077a599f5767c01b53441
Taylor Young <tdy721@gmail.com>
2015-01-20 16:02:51 -0600

    reportmaker is CHARTs + TABLEs

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,52 @@
 var HOST = 'http://yothrow.com';
 
+var chartOptions = {
+  showPoint: false,
+  lineSmooth: false,
+  showArea: true,
+  height: 200
+};
+
+var responsiveChartOptions = {};
+
 $.ajaxSetup({ data: { 'testing' : 'supersecret' } });
 
-$('#queryOptions').on( 'change', makeTable );
+// $('#queryOptions').on( 'change', makeTable );
 
 // Show Query List Dropdown, then renderTable
 $.ajax({
   url: HOST + '/api/query'
 })
 .done( function ( data ) {
-  tmpl.renderTemplate ( '#queryOptions', data, 'dropdown', [], makeTable );
+  tmpl.renderTemplate( '#queryOptions', data, 'dropdown', [], function () {
+    $('#queryOptions').change();
+    makeTable();
+  });
 })
 .fail( alertError );
+
+// Static AJAX reqest for GAAPI data
+$('body').on( 'change', '#queryOptions', function ( e ) {
+  makeTable();
+  returnQueryJSON( $('#queryOptions').val(), function ( data ) {
+    if ( data.rows.length === 1 ) {
+      tmpl.renderTemplate( '#chartButtons', { pie: true, bar: true, line: false }, 'chart_buttons' );
+      renderChart( '#theChart', data, 'pie' );
+    }
+    else {
+      tmpl.renderTemplate( '#chartButtons', { pie: false, bar: true, line: true }, 'chart_buttons' );
+      renderChart( '#theChart', data, 'line' );
+    }
+  });
+  return false;
+});
+
+$('body').on( 'click', '.makeChartButton', function ( e ) {
+  returnQueryJSON( $('#queryOptions').val(), function ( data ) {
+    renderChart( '#theChart', data, $( e.currentTarget ).val() );
+  });
+  return false;
+});
 
 function makeTable () {
   $.ajax({
@@ -28,4 +63,19 @@ function makeTable () {
 function alertError ( err ) {
   if ( err !== undefined )
     alert( 'AJAX Error: ' + JSON.stringify( err ) );
+}
+
+function renderChart ( selector, data, chartType ) {
+  // $( selector ).html('');
+  tmpl.renderTemplate( selector, { id: 'Chart42' }, 'chart', [], function () {
+    chart[ chartType ]( '#Chart42', data );
+  });
+}
+
+function returnQueryJSON ( id, callback ) {
+  $.ajax({
+    url: HOST + '/api/result/' + id
+  })
+  .done( callback )
+  .fail( alertError );
 }
