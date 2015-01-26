d11b15944e8f15a1c05750acc4f1d9b9c39d116e
Taylor Young <tdy721@gmail.com>
2015-01-26 08:23:45 -0600

    things

--- src/js/script.js
+++ src/js/script.js
@@ -9,6 +9,14 @@ var chartOptions = {
 
 var responsiveChartOptions = {};
 
+var block = {
+  query_id: '',
+  chart: true,
+  chart_type: 'pie',
+  table: true,
+  insight: ''
+};
+
 $.ajaxSetup({ data: { 'testing' : 'supersecret' } });
 
 // $('#queryOptions').on( 'change', makeTable );
@@ -20,23 +28,47 @@ $.ajax({
 .done( function ( data ) {
   tmpl.renderTemplate( '#queryOptions', data, 'dropdown', [], function () {
     $('#queryOptions').change();
-    makeTable();
   });
 })
 .fail( alertError );
 
+$('#chart').on( 'change', function ( e ) {
+  block.chart = $('#chart').is(':checked');
+  $('#queryOptions').change();
+  return false;
+});
+
+$('#table').on( 'change', function ( e ) {
+  block.table = $('#table').is(':checked');
+  $('#queryOptions').change();
+  return false;
+});
+
 // Static AJAX reqest for GAAPI data
 $('body').on( 'change', '#queryOptions', function ( e ) {
-  makeTable();
+
   returnQueryJSON( $('#queryOptions').val(), function ( data ) {
-    if ( data.rows.length === 1 ) {
-      tmpl.renderTemplate( '#chartButtons', { pie: true, bar: true, line: false }, 'chart_buttons' );
-      renderChart( '#theChart', data, 'pie' );
+
+    block.query_id = $('#queryOptions').val();
+    console.log( block );
+
+    if ( block.chart ) {
+      if ( data.rows.length === 1 ) {
+        tmpl.renderTemplate( '#chartButtons', { pie: true, bar: true, line: false }, 'chart_buttons' );
+        renderChart( '#theChart', data, 'pie' );
+      }
+      else {
+        tmpl.renderTemplate( '#chartButtons', { pie: false, bar: true, line: true }, 'chart_buttons' );
+        renderChart( '#theChart', data, 'line' );
+      }
     }
-    else {
-      tmpl.renderTemplate( '#chartButtons', { pie: false, bar: true, line: true }, 'chart_buttons' );
-      renderChart( '#theChart', data, 'line' );
+
+    if ( block.table ) {
+      tmpl.renderTemplate ( '#tableHolder', data, 'table', [ 'table_head', 'table_body' ], function () {
+        $('.tablesaw').table().data( "table" ).refresh();
+      });
     }
+
   });
   return false;
 });
@@ -48,17 +80,14 @@ $('body').on( 'click', '.makeChartButton', function ( e ) {
   return false;
 });
 
-function makeTable () {
-  $.ajax({
-    url: HOST + '/api/result/' + $('#queryOptions').val()
-  })
-  .done( function ( data ) {
-    tmpl.renderTemplate ( '#tableHolder', data, 'table', [ 'table_head', 'table_body' ], function () {
-      $('.tablesaw').table().data( "table" ).refresh();
-    });
-  })
-  .fail( alertError );
-}
+// function makeTable () {
+//   $.ajax({
+//     url: HOST + '/api/result/' + $('#queryOptions').val()
+//   })
+//   .done( function ( data ) {
+//   })
+//   .fail( alertError );
+// }
 
 function alertError ( err ) {
   if ( err !== undefined )
