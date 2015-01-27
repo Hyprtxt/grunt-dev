08add5b472fd68c80ab2e85ca3044039ff76fd66
Taylor Young <tdy721@gmail.com>
2015-01-27 08:34:12 -0600

    basic markup in place

--- config.js
+++ config.js
@@ -1,23 +1,13 @@
 module.exports = {
-	title: "Reap Report Maker"
-	, description: "A Reap Marketing Application"
-	, heading: "Report Maker Beta 0.5.0"
+	title: "Bell'Invito Landing Page"
+	, description: ""
+	, heading: "You Design. We Print."
 	, javascripts: [
 		'bower_components/jquery/dist/jquery.js'
-		, 'bower_components/jsviews/jsviews.js'
-		, 'bower_components/tablesaw/dist/tablesaw.js'
-		, 'bower_components/chartist/dist/chartist.js'
-		, 'bower_components/pace/pace.js'
-		, 'dev/js/tmpl.js'
-		, 'dev/js/chart.js'
 		, 'dev/js/script.js'
 	]
 	, stylesheets: [
 		'bower_components/bootstrap/dist/css/bootstrap.css'
-		, 'bower_components/pace/themes/green/pace-theme-barber-shop.css'
-		, 'bower_components/tablesaw/dist/tablesaw.css'
-		, 'bower_components/chartist/dist/chartist.min.css'
-		, 'dev/css/futura.css'
 		, 'dev/css/style.css'
 	]
 };
